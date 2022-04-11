8bd4793dcaff8fc81d071b506f57a4895d19ede7
Ruben <hartensteinruben@gmail.com>
2022-04-11 18:44:07 +0200

    The first step (forward)

--- MGPummelz/Assets/minigame/pummelz/ai/NobleSweetPaprika/MGPumNobleSweetPaprikaAIPlayerController.cs
+++ MGPummelz/Assets/minigame/pummelz/ai/NobleSweetPaprika/MGPumNobleSweetPaprikaAIPlayerController.cs
@@ -16,7 +16,24 @@ namespace mg.pummelz
 
         internal override MGPumCommand calculateCommand()
         {
-            return new MGPumEndTurnCommand(this.playerID);
+            MGPumMoveCommand moveCommand = null;
+            List<MGPumUnit> availableUnits = state.getAllUnitsInZone(MGPumZoneType.Battlegrounds, this.playerID);
+            foreach (MGPumUnit unit in availableUnits)
+            {
+                if (!stateOracle.canMove(unit) || unit.currentSpeed < 1)
+                    continue;
+                MGPumField targetField = state.getField(new Vector2Int(unit.field.x, unit.field.y) + Vector2Int.up);
+                if (targetField == null || !targetField.isEmpty())
+                    continue;
+                
+                MGPumFieldChain chain = new MGPumFieldChain(this.playerID, unit.getMoveMatcher());
+                if(chain.canAdd(unit.field))
+                    chain.add(unit.field);
+                if(chain.canAdd(targetField))
+                    chain.add(targetField);
+                moveCommand = new MGPumMoveCommand(this.playerID, chain, unit);
+            }
+            return moveCommand;
         }
     }
 }
