2fad1fa7f4e12ac979c53400a5cc65961cf7fdf0
Ruben <hartensteinruben@gmail.com>
2022-04-30 17:07:24 +0200

    Generating all possible Moves and executing a random one

---
+++ MGPummelz/Assets/minigame/pummelz/ai/NobleSweetPaprika/MGPumBreadthFirstSearch.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace mg.pummelz
+{
+    public class MGPumBreadthFirstSearch 
+    {
+        public bool[,] queued;
+        public Queue<Vector2Int> tilesToVisit;
+        public Dictionary<Vector2Int, Vector2Int> predecessors;
+
+        public List<Vector2Int> getNeighbors(Vector2Int pos, MGPumFields fields)
+        {
+            List<Vector2Int> neighbourFields = new List<Vector2Int>();
+            MGPumField home = fields.getField(pos);
+            foreach(MGPumField field in fields.fieldArray) 
+            {
+                if(home.neighboring(field))
+                    neighbourFields.Add(field.coords);
+            }
+            return neighbourFields;
+        }
+
+        public List<Vector2Int> findPathInternal(MGPumUnit unit, MGPumField targetField, MGPumFields fields)
+        {
+            queued = new bool[fields.dimSize, fields.dimSize];
+            tilesToVisit = new Queue<Vector2Int>();
+            predecessors = new Dictionary<Vector2Int, Vector2Int>();
+
+            tilesToVisit.Enqueue(unit.field.coords);
+            queued[unit.field.x, unit.field.y] = true;
+
+            int recursion = 0;
+            int maxRecursion = 500;
+            while(tilesToVisit.Count > 0)
+            {
+                recursion++;
+                if(recursion > maxRecursion)
+                    break;
+                
+                Vector2Int position = tilesToVisit.Dequeue();
+                
+                if (fields.fieldArray[position.x, position.y] == null || (!fields.getField(position).isEmpty() && position != unit.field.coords)){
+                    continue; 
+                }
+
+                if (position == targetField.coords)
+                {
+                    List<Vector2Int> path = new List<Vector2Int>();
+                    path.Add(position);
+
+                    //reconstruct path in reverse
+                    while(predecessors.ContainsKey(path[0]))
+                    {
+                        path.Insert(0, predecessors[path[0]]);
+                    }
+                    return path;
+                }
+
+                foreach (Vector2Int neighbor in getNeighbors(position, fields))
+                {
+                    if(!queued[neighbor.x, neighbor.y])
+                    {
+                        queued[neighbor.x, neighbor.y] = true;
+                        tilesToVisit.Enqueue(neighbor);
+                        predecessors.Add(neighbor, position);
+                    }
+                }
+            }
+            return null;     
+        }
+    }
+}
--- MGPummelz/Assets/minigame/pummelz/ai/NobleSweetPaprika/MGPumNobleSweetPaprikaAIPlayerController.cs
+++ MGPummelz/Assets/minigame/pummelz/ai/NobleSweetPaprika/MGPumNobleSweetPaprikaAIPlayerController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace mg.pummelz
@@ -7,33 +8,114 @@ namespace mg.pummelz
     {
 
         public const string type = "NobleSweetPaprika";
+        public MGPumBreadthFirstSearch pathFinder = new MGPumBreadthFirstSearch();
         protected override int[] getTeamMartikels()
         {
-            return new int[]{2746235, 8366074};
+            return new int[] { 2746235, 8366074 };
         }
 
-        public MGPumNobleSweetPaprikaAIPlayerController(int playerID) : base(playerID){}
+        public MGPumNobleSweetPaprikaAIPlayerController(int playerID) : base(playerID) { }
 
         internal override MGPumCommand calculateCommand()
         {
-            MGPumMoveCommand moveCommand = null;
+            MGPumCommand moveCommand = new MGPumEndTurnCommand(this.playerID);
+            List<MGPumCommand> commands = new List<MGPumCommand>();
             List<MGPumUnit> availableUnits = state.getAllUnitsInZone(MGPumZoneType.Battlegrounds, this.playerID);
             foreach (MGPumUnit unit in availableUnits)
-            {
-                if (!stateOracle.canMove(unit) || unit.currentSpeed < 1)
-                    continue;
-                MGPumField targetField = state.getField(new Vector2Int(unit.field.x, unit.field.y) + Vector2Int.up);
-                if (targetField == null || !targetField.isEmpty())
-                    continue;
-                
-                MGPumFieldChain chain = new MGPumFieldChain(this.playerID, unit.getMoveMatcher());
-                if(chain.canAdd(unit.field))
-                    chain.add(unit.field);
-                if(chain.canAdd(targetField))
-                    chain.add(targetField);
-                moveCommand = new MGPumMoveCommand(this.playerID, chain, unit);
+            {  
+                commands.AddRange(getAllCommands(unit));
             }
+            if (commands.Count == 0)
+            {
+                return moveCommand;
+            }
+            MGPumOracleBoolResult result = this.stateOracle.checkCommandDetailed(commands[0], false);
+            if (result.result)
+            {
+                return commands[rng.Next(commands.Count)];
+            }
+
             return moveCommand;
+        }
+
+        private List<MGPumCommand> getAllCommands(MGPumUnit unit)
+        {
+            List<MGPumCommand> commands = new List<MGPumCommand>();
+            if (stateOracle.canMove(unit) && unit.currentSpeed >= 1)
+            {
+                commands.AddRange(getAllMoveCommands(unit));
+            }
+
+            //if (stateOracle.canAttack(unit) && unit.currentRange >= 1)
+            //{
+            //    Debug.Log("Can Attack");
+            //    commands.AddRange(getAllAttackCommands(unit));
+            //}
+
+            return commands;
+        }
+
+        private List<MGPumMoveCommand> getAllMoveCommands(MGPumUnit unit)
+        {
+            List<MGPumMoveCommand> moveCommands = new List<MGPumMoveCommand>();
+            MGPumField[,] fields = this.state.fields.fieldArray;
+            foreach (MGPumField field in fields)
+            {
+                if (field == null || !field.isEmpty())
+                    continue;
+                int dist = getAbsoluteDistance(unit.field, field);
+                if (dist <= unit.currentSpeed)
+                {   
+                    MGPumMoveCommand command = getMoveCommandBreadthFirst(unit, field);
+                    if (command != null)
+                    {
+                        moveCommands.Add(command);
+                    }                        
+                }
+            }
+            return moveCommands;
+        }
+
+        private List<MGPumAttackCommand> getAllAttackCommands(MGPumUnit unit)
+        {
+            List<MGPumAttackCommand> attackCommands = new List<MGPumAttackCommand>();
+            return attackCommands;
+        }
+
+        private int getAbsoluteDistance(MGPumField here, MGPumField there)
+        {
+            int x1 = here.x;
+            int y1 = here.y;
+            int x2 = there.x;
+            int y2 = there.y;
+            return Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+        }
+
+        private MGPumMoveCommand getMoveCommandBreadthFirst(MGPumUnit unit, MGPumField targetField)
+        {
+            MGPumFieldChain chain = new MGPumFieldChain(this.playerID, unit.getMoveMatcher());
+            List<Vector2Int> path = pathFinder.findPathInternal(unit, targetField, this.state.fields);
+            if (path == null)
+            {
+                return null;
+            }
+            foreach (Vector2Int coords in path)
+            {
+                MGPumField field = this.state.fields.getField(coords);
+                if (chain.canAdd(field))
+                {
+                    chain.add(field);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            if(chain.isValidChain())
+            {
+                return new MGPumMoveCommand(this.playerID, chain, unit);  
+            }
+            return null;
         }
     }
 }
