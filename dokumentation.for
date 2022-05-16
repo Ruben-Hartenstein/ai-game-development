b883dc38e4b86a14bc2bf068711f61273f2bc0a3
Jona <krumrein.jona@gmail.com>
2022-05-16 17:21:23 +0200

    added LaTeX template

---
+++ dokumentation.for
@@ -0,0 +1,14 @@
+\babel@toc {ngerman}{}\relax 
+\babel@toc {ngerman}{}\relax 
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\contentsline {formel}{\numberline {1.1} Beispielformel}{2}{equation.1.1.1}%
+\defcounter {refsection}{0}\relax 
+\addvspace {10\p@ }
+\defcounter {refsection}{0}\relax 
+\providecommand \tocbasic@end@toc@file {}\tocbasic@end@toc@file 
